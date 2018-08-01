8c8ae5ad0d66af48f6fa565f4e0ecbf0bf0661bc
hspark <hspark@cs.umn.edu>
2018-08-01 00:56:09 -0500

    Matching_sift/

---
+++ Matching_sift/build/CMakeFiles/Matching_sift.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hspark/sfm_github/Matching_sift/DataUtility.cpp" "/home/hspark/sfm_github/Matching_sift/build/CMakeFiles/Matching_sift.dir/DataUtility.cpp.o"
+  "/home/hspark/sfm_github/Matching_sift/MathUtility.cpp" "/home/hspark/sfm_github/Matching_sift/build/CMakeFiles/Matching_sift.dir/MathUtility.cpp.o"
+  "/home/hspark/sfm_github/Matching_sift/matching.cpp" "/home/hspark/sfm_github/Matching_sift/build/CMakeFiles/Matching_sift.dir/matching.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Matching_sift/build/CMakeFiles/Matching_sift.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matching_sift.dir/matching.cpp.o"
+  "CMakeFiles/Matching_sift.dir/MathUtility.cpp.o"
+  "CMakeFiles/Matching_sift.dir/DataUtility.cpp.o"
+  "Matching_sift.pdb"
+  "Matching_sift"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matching_sift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
