8a027c576da6c49538b55f22c7a23b38b41a8b8c
hspark <hspark@cs.umn.edu>
2018-08-07 01:36:21 -0500

    sfm

---
+++ SfM/build/CMakeFiles/SfM.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hspark/sfm_github/SfM/CeresUtility.cpp" "/home/hspark/sfm_github/SfM/build/CMakeFiles/SfM.dir/CeresUtility.cpp.o"
+  "/home/hspark/sfm_github/SfM/DataUtility.cpp" "/home/hspark/sfm_github/SfM/build/CMakeFiles/SfM.dir/DataUtility.cpp.o"
+  "/home/hspark/sfm_github/SfM/MathUtility.cpp" "/home/hspark/sfm_github/SfM/build/CMakeFiles/SfM.dir/MathUtility.cpp.o"
+  "/home/hspark/sfm_github/SfM/SfM.cpp" "/home/hspark/sfm_github/SfM/build/CMakeFiles/SfM.dir/SfM.cpp.o"
+  "/home/hspark/sfm_github/SfM/epnp.cpp" "/home/hspark/sfm_github/SfM/build/CMakeFiles/SfM.dir/epnp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SfM/build/CMakeFiles/SfM.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SfM.dir/SfM.cpp.o"
+  "CMakeFiles/SfM.dir/CeresUtility.cpp.o"
+  "CMakeFiles/SfM.dir/DataUtility.cpp.o"
+  "CMakeFiles/SfM.dir/epnp.cpp.o"
+  "CMakeFiles/SfM.dir/MathUtility.cpp.o"
+  "SfM.pdb"
+  "SfM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SfM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
